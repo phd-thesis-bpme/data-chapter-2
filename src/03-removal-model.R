7fc6491004dd0dd4c255794913812d32e4d6ed6e
Brandon Edwards <brandon.edwards144@gmail.com>
2022-08-05 11:33:01 -0400

    update removal modelling script call to work with cmdstan

--- src/03-removal-model.R
+++ src/03-removal-model.R
@@ -7,9 +7,9 @@
 
 ####### Import Libraries and External Files #######
 
-library(rstan)
-options(mc.cores = parallel::detectCores())
-rstan_options(auto_write = TRUE)
+library(cmdstanr)
+#options(mc.cores = parallel::detectCores())
+#rstan_options(auto_write = TRUE)
 
 ####### Read Data #################################
 
@@ -17,20 +17,29 @@ load("data/generated/removal_stan_data.rda")
 
 ####### Stan Modelling ############################
 
-model <- stan_model(file = "models/removal.stan")
+model <- cmdstan_model(stan_file = "models/removal.stan",
+                       cpp_options = list(stan_threads = TRUE))#model <- stan_model(file = "models/removal.stan")
 
-removal_stan_job <- sampling(model,
-                            data = removal_stan_data,
-                            verbose = TRUE,
-                            chains = 4,
-                            iter = 1000,
-                            warmup = 500,
-                            cores = 4,
-                            pars = c("log_phi", "sigma", "mu"),
-                            control = list(adapt_delta = 0.8,
-                                           max_treedepth = 15))
+removal_stan_fit <- model$sample(
+  data = removal_stan_data,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 100
+)
+
+# removal_stan_job <- sampling(model,
+#                             data = removal_stan_data,
+#                             verbose = TRUE,
+#                             chains = 4,
+#                             iter = 1000,
+#                             warmup = 500,
+#                             cores = 4,
+#                             pars = c("log_phi", "sigma", "mu"),
+#                             control = list(adapt_delta = 0.8,
+#                                            max_treedepth = 15))
 
 ####### Output ####################################
 
-save(removal_stan_job, file = "data/generated/removal_model.rda")
+fit$save_object(file = "data/generated/removal_model.RDS")
+#save(removal_stan_fit, file = "data/generated/removal_model.rda")
 
