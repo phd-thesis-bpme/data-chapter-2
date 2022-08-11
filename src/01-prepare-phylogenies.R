7e07148cf4b0347eb76832314bec9dc393e3f51c
Brandon Edwards <brandon.edwards144@gmail.com>
2022-08-11 16:34:00 -0400

    file tempalte for prepping phylogenies

---
+++ src/01-prepare-phylogenies.R
@@ -0,0 +1,17 @@
+####### Script Information ########################
+# Brandon P.M. Edwards
+# Multi-species QPAD Detectability
+# 01-prepare-phylogenies.R
+# Created August 2022
+# Last Updated August 2022
+
+####### Import Libraries and External Files #######
+
+
+####### Read Data #################################
+
+
+####### Main Code #################################
+
+
+####### Output ####################################
