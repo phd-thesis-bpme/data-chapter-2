6bd75aed4c9d2ccb34c5e65fb42c2555979da7ec
Brandon Edwards <brandon.edwards144@gmail.com>
2022-10-28 09:40:34 -0600

    Coded small version of full model run (i.e., not the cross validation part yet) to test it on the server. This only includes 10 warmups and 50 samples

---
+++ src/03-removal-model-cv.R
@@ -0,0 +1,47 @@
+####### Script Information ########################
+# Brandon P.M. Edwards
+# Multi-species QPAD Detectability
+# 02-removal-model-cv.R
+# Created April 2022
+# Last Updated October 2022
+
+####### Import Libraries and External Files #######
+
+library(cmdstanr)
+
+####### Load Data #################################
+
+load("data/generated/removal_stan_data_cv.rda")
+
+####### Cross Validation ##########################
+
+models <- c("brownian", "ou")
+
+
+for (m in models)
+{
+  dir.create(paste0("output/cv/removal/", m), recursive = TRUE)
+  model_file <- cmdstan_model(stan_file = paste0("models/removal_", m, ".stan"),
+                         cpp_options = list(stan_threads = TRUE))
+  removal_stan_data_cv$grainsize <- 1
+  
+  # first, run the full model with no species removed
+  removal_stan_fit_full <- model_file$sample
+  (
+    data = removal_stan_data,
+    iter_warmup = 10,
+    iter_sampling = 50,
+    chains = 4,
+    parallel_chains = 4,
+    refresh = 10,
+    threads_per_chain = 4
+  )
+  
+  removal_stan_fit_full$save_object(file = paste0("output/cv/removal/", m,"/1full.RDS"))
+}
+
+####### Output ####################################
+
+
+#save(removal_stan_fit, file = "data/generated/removal_model.rda")
+
