508147df093e56bef080d7683e7e8f499cabafde
Brandon Edwards <brandon.edwards144@gmail.com>
2022-10-28 15:36:30 -0600

    Script to run full cross-validation for all species, for only brownian and pagel models for now. O-U will need some work, or some thought as to whether or not to keep it.

--- src/03-removal-model-cv.R
+++ src/03-removal-model-cv.R
@@ -1,7 +1,7 @@
 ####### Script Information ########################
 # Brandon P.M. Edwards
 # Multi-species QPAD Detectability
-# 02-removal-model-cv.R
+# 03-removal-model-cv.R
 # Created April 2022
 # Last Updated October 2022
 
@@ -13,14 +13,23 @@ library(cmdstanr)
 
 load("data/generated/removal_stan_data_cv.rda")
 
+####### Set Constants #############################
+
+models <- c("brownian", "pagel")
+removal_stan_data_cv$grainsize <- 1
+removal_stan_data_cv$lambda <- 0.76
+
+# Stan settings
+n_iter <- 2000
+n_warmup <- 1000
+n_chains <- 4
+refresh <- 100
+threads_per_chain <- 4
+
 ####### Cross Validation ##########################
 
 sp_list <- removal_stan_data_cv$sp_list
 removal_stan_data_cv$sp_list <- NULL
-models <- c("pagel")
-removal_stan_data_cv$grainsize <- 1
-removal_stan_data_cv$lambda <- 0.76
-
 
 for (m in models)
 {
@@ -31,18 +40,19 @@ for (m in models)
   # first, run the full model with no species removed
   removal_stan_fit_full <- model_file$sample(
     data = removal_stan_data_cv,
-    iter_warmup = 10,
-    iter_sampling = 50,
-    chains = 4,
-    parallel_chains = 4,
-    refresh = 10,
-    threads_per_chain = 4
+    iter_warmup = n_warmup,
+    iter_sampling = n_iter,
+    chains = n_chains,
+    parallel_chains = n_chains,
+    refresh = refresh,
+    threads_per_chain = threads_per_chain
   )
   removal_stan_fit_full$save_object(file = paste0("output/cv/removal/", m,"/1full.RDS"))
+  rm(removal_stan_fit_fill)
   
   sp_list_unique <- unique(sp_list)[,1]
   
-  for (sp in sp_list_unique[1:3])
+  for (sp in sp_list_unique)
   {
     #' Figure out which indices need to be removed, and create a new data list
     #' with those particular indices removed from each matrix or vector.
@@ -65,17 +75,18 @@ for (m in models)
     
     removal_stan_fit_sp <- model_file$sample(
       data = removal_stan_data_cv_sp,
-      iter_warmup = 10,
-      iter_sampling = 50,
-      chains = 4,
-      parallel_chains = 4,
-      refresh = 10,
-      threads_per_chain = 4
+      iter_warmup = n_warmup,
+      iter_sampling = n_iter,
+      chains = n_chains,
+      parallel_chains = n_chains,
+      refresh = refresh,
+      threads_per_chain = threads_per_chain
     )
     removal_stan_fit_sp$save_object(file = paste0("output/cv/removal/",
                                                   m,
                                                   "/",
                                                   sp,
                                                   ".RDS"))
+    rm(removal_stan_fit_sp)
   }
 }
