3215862d3b109721fe6be72ec669e34f2f1023df
Brandon Edwards <brandon.edwards144@gmail.com>
2023-02-21 20:37:02 -0500

    incomplete prior predictive check for distance sampling

--- src/05-distance-model.R
+++ src/05-distance-model.R
@@ -8,6 +8,10 @@
 ####### Import Libraries and External Files #######
 
 library(cmdstanr)
+library(MASS)
+library(ggplot2)
+library(ggpubr)
+theme_set(theme_pubclean())
 
 ####### Load Data #################################
 
@@ -27,6 +31,129 @@ n_warmup <- 1000
 n_chains <- 4
 refresh <- 10
 threads_per_chain <- 3
+
+# Prior predictive check settings
+n_sims <- 100
+
+####### Prior Predictive Check ####################
+
+phylo_corr_pl <- distance_stan_data_pred$phylo_corr * distance_stan_data_pred$lambda
+for (i in 1:dim(phylo_corr_pl)[1])
+{
+  phylo_corr_pl[i,i] <- 1
+}
+
+# Sigma
+sigma <- rexp(n = n_sims, rate = 1)
+pdf(file = "output/prior_predictive_check/distance/sigma.pdf")
+print(ggplot(data = data.frame(sigma), aes(x = sigma)) +
+        geom_histogram(bins = 20) +
+        NULL)
+dev.off()
+
+# mu mig strat
+mu_mig_strat <- matrix(data = NA,
+                       ncol = distance_stan_data_pred$n_mig_strat,
+                       nrow = n_sims)
+for (i in 1:removal_stan_data_pred$n_mig_strat)
+{
+  mu_mig_strat[,i] <- rnorm(n_sims, mean = 0, sd = 0.5)
+}
+pdf(file = "output/prior_predictive_check/distance/mu_mig_strat.pdf")
+to_plot <- data.frame(Value = c(mu_mig_strat[,1],
+                                mu_mig_strat[,2]),
+                      Mig_Strat = c(rep("Resident", 100),
+                                    rep("Migrant", 100)))
+for (i in unique(to_plot$Mig_Strat))
+{
+  print(ggplot(data = to_plot[which(to_plot$Mig_Strat == i), ],
+               aes(x = Value)) +
+          geom_histogram(bins = 20) +
+          xlab(i) +
+          xlim(floor(min(to_plot$Value) - 1), ceiling(max(to_plot$Value) + 1)) +
+          NULL)
+}
+dev.off()
+
+# mu habitat
+mu_habitat <- matrix(data = NA,
+                       ncol = distance_stan_data_pred$n_habitat,
+                       nrow = n_sims)
+for (i in 1:distance_stan_data_pred$n_habitat)
+{
+  mu_habitat[,i] <- rnorm(n_sims, mean = 0, sd = 0.5)
+}
+pdf(file = "output/prior_predictive_check/distance/mu_habitat.pdf")
+to_plot <- data.frame(Value = c(mu_habitat[,1],
+                                mu_habitat[,2]),
+                      Habitat = c(rep("Open", 100),
+                                    rep("Closed", 100)))
+for (i in unique(to_plot$Habitat))
+{
+  print(ggplot(data = to_plot[which(to_plot$Habitat == i), ],
+               aes(x = Value)) +
+          geom_histogram(bins = 20) +
+          xlab(i) +
+          xlim(floor(min(to_plot$Value) - 1), ceiling(max(to_plot$Value) + 1)) +
+          NULL)
+}
+dev.off()
+
+# beta mass
+beta_mass <- rnorm(n = n_sims, mean = 0, sd = 1)
+pdf(file = "output/prior_predictive_check/distance/beta_mass.pdf")
+print(ggplot(data = data.frame(sigma), aes(x = sigma)) +
+        geom_histogram(bins = 20) +
+        NULL)
+dev.off()
+
+mu <- matrix(data = NA, nrow = n_sims, ncol = removal_stan_data_pred$n_species)
+pdf(file = "output/prior_predictive_check/removal/mu.pdf")
+for (s in 1:removal_stan_data_pred$n_species)
+{
+  mu[,s] <- rnorm(n = n_sims,
+                  mean = mu_mig_strat[, removal_stan_data_pred$mig_strat[s]])
+  to_plot <- data.frame(Value = mu[,s])
+  print(ggplot(data = to_plot, aes(x = Value)) +
+          geom_histogram(bins = 20) +
+          xlab(dimnames(phylo_corr_pl)[[1]][s]) +
+          xlim(floor(min(mu) - 1), ceiling(max(mu) + 1)) +
+          NULL)
+}
+
+dev.off()
+
+log_phi <- matrix(data = NA, nrow = n_sims, ncol = removal_stan_data_pred$n_species)
+for (i in 1:n_sims)
+{
+  log_phi[i,] <- MASS::mvrnorm(n = 1, mu = mu[i,],
+                               Sigma = phylo_corr_pl * sigma[i])
+}
+
+pdf(file = "output/prior_predictive_check/removal/log_phi.pdf")
+for (s in 1:removal_stan_data_pred$n_species)
+{
+  to_plot <- data.frame(Value = (log_phi[,s]))
+  print(ggplot(data = to_plot, aes(x = Value)) +
+          geom_histogram(bins = 20) +
+          xlab(dimnames(phylo_corr_pl)[[1]][s]) +
+          xlim(floor(min(log_phi) - 1), ceiling(max(log_phi) + 1)) +
+          NULL)
+}
+dev.off()
+
+pdf(file = "output/prior_predictive_check/removal/phi.pdf")
+phi <- exp(log_phi)
+for (s in 1:removal_stan_data_pred$n_species)
+{
+  to_plot <- data.frame(Value = (phi[,s]))
+  print(ggplot(data = to_plot, aes(x = Value)) +
+          geom_histogram(bins = 20) +
+          xlab(dimnames(phylo_corr_pl)[[1]][s]) +
+          xlim(0, 10) +
+          NULL)
+}
+dev.off()
 
 ####### Run Model #################################
 
