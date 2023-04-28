57dc115d95d45bf12cbd01971ce135291b5c1baf
Brandon Edwards <brandon.edwards144@gmail.com>
2023-04-28 13:05:25 -0400

    post hoc analysis scripts for distance modelling

--- src/07-distance-analysis.R
+++ src/07-distance-analysis.R
@@ -24,61 +24,128 @@ load("data/generated/distance_stan_data_pred.rda")
 
 ####### Main Code #################################
 
-species <- rownames(corr_matrix)
-species <- gsub("_", " ", species)
-bin_red <- binomial[which(binomial$Scientific %in% species), ]
-bin_red <- bin_red[match(species, bin_red$Scientific), ]
-codes <- bin_red$Code
+# Extract log_phi summary statistics from full Stan model runs
+dis_summary <- dis_model$summary("log_tau")
 
-# Create empty data frame
-distance_df <- data.frame(Species = rep(codes, 2),
-                         N = NA,
-                         Estimate = NA,
-                         Lower = NA,
-                         Upper = NA,
-                         Model = c(rep("Single", 11), rep("Multi", 11)))
-stan_summary <- data.frame(summary(stan_job)$summary)
+# Model diagnostics
 
-i <- 1
-for (sp in codes)
+# diag <- dis_model$sampler_diagnostics(format = "df")
+# sigma_draws <- dis_model$draws(variables = "mu_mig_strat[1]", format = "df")
+# sigma_draws$.chain <- factor(sigma_draws$.chain, levels = c("1", "2", "3", "4"))
+# ggplot(data = sigma_draws, aes(x = .iteration, y = mu, group = (.chain), color = .chain)) +
+#   geom_line()
+# mcmc_hist(dis_model$draws("sigma")) 
+
+# Add species names to these summaries
+dis_summary$Scientific_BT <- gsub("_", " ", rownames(corr_matrix_predict))
+dis_summary <- join(dis_summary, binomial[, c("Scientific_BT", "Code")], by = "Scientific_BT")
+
+# Get data sample size for all species and add to summary
+species_n <- data.frame(table(distance_stan_data_pred$species))
+names(species_n) <- c("index", "N")
+dis_summary$index <- seq(1, nrow(dis_summary))
+dis_summary$N <- 0
+for (i in 1:nrow(species_n))
 {
-  napops_est <- napops::edr(species = sp, model = 1, forest = c(0.0,0.5,1.0), road = TRUE, quantiles = c(0.025, 0.975))
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Single"), "Estimate"] <- napops_est$EDR_est[1]
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Single"), "Lower"] <- napops_est$EDR_2.5[1]
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Single"), "Upper"] <- napops_est$EDR_97.5[1]
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Single"), "N"] <- nrow(covariates_distance(species = sp))
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Multi"), "N"] <- nrow(covariates_distance(species = sp))
-  
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Multi"), "Estimate"] <- exp(stan_summary$mean[i])
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Multi"), "Lower"] <- exp(stan_summary$X2.5.[i])
-  distance_df[which(distance_df$Species == sp & 
-                     distance_df$Model == "Multi"), "Upper"] <- exp(stan_summary$X97.5.[i])
-  
-  i <- i + 1
+  dis_summary[which(dis_summary$index == species_n$index[i]), "N"] <-
+    species_n$N[i]
 }
 
-distance_df$Species <- factor(distance_df$Species, levels = codes)
-distance_df$Model <- factor(distance_df$Model, levels = c("Single", "Multi"))
-distance_df$Log_N <- log(distance_df$N)
+# Get original single-species NA-POPS estimates
+napops_summary <- napops::coef_distance(species = dis_summary$Code, model = 1)
 
-# Plot single vs multi
-comparison_plot <- ggplot(data = distance_df, aes(x = Species, y = Estimate, group = Model, color = Model)) +
-  geom_point(aes(size = Log_N), position = position_dodge(width = 0.4)) +
-  geom_errorbar(aes(ymin = Lower, ymax = Upper), width = 0, position = position_dodge(width = 0.4)) +
-  ylab("EDR") +
+####### 1-to-1 Plot ###############################
+
+to_plot <- merge(dis_summary[,c("Code", "mean")], napops_summary[, c("Species", "Intercept")],
+                 by.x = "Code", by.y = "Species")
+names(to_plot) <- c("Species", "Multi", "Single")
+to_plot$Label <- ""
+to_plot$diff <- NA
+for (i in 1:nrow(to_plot))
+{
+  to_plot$diff[i] <- abs(to_plot$Multi[i] - to_plot$Single[i])
+  if (to_plot$diff[i] > 0.1)
+  {
+    to_plot$Label[i] <- to_plot$Species[i]
+  }
+}
+single_vs_multi_plot <- ggplot(data = to_plot, mapping = aes(x = exp(Single), y = exp(Multi))) +
+  geom_point() +
+  geom_abline(slope = 1) +
+  # xlim(0,1) +
+  # ylim(0,1) +
+  geom_text(aes(label = Label)) +
   NULL
 
+####### SD Comparison Plot ########################
+
+to_plot <- dis_summary[which(dis_summary$Code %in% napops_summary$Species), 
+                       c("Code", "N", "sd")]
+names(to_plot) <- c("Code", "N", "SD_Multi")
+to_plot$SD_Single <- NA
+# Get original NA-POPS SDs
+for (i in 1:nrow(to_plot))
+{
+  to_plot$SD_Single[i] <- sqrt(napops::get_vcv(species = to_plot$Code[i],
+                                               model_type = "dis",
+                                               model_num = 1))
+}
+to_plot_long <- reshape2::melt(to_plot, id = c("Code", "N"), variable_name = "Model")
+
+sd_model_data <- list(n_samples = nrow(to_plot_long),
+                      stdev = to_plot_long$value,
+                      N = to_plot_long$N,
+                      model_type = factor(to_plot_long$variable,
+                                          levels = c("SD_Single", "SD_Multi")))
+
+sd_model <- cmdstan_model(stan_file = "models/sd_model.stan")
+
+sd_model_run <- sd_model$sample(
+  data = sd_model_data,
+  iter_warmup = 1000,
+  iter_sampling = 2000,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 10
+)
+
+sd_comp_plot <- ggplot(data = to_plot_long, 
+                       aes(x = log(N), y = (value),
+                           group = variable, color = variable)) + 
+  geom_smooth() +
+  xlab("log(Sample Size)") +
+  ylab("Standard Deviation") +
+  NULL
+
+####### Species-specific Plots ####################
+
+sp <- c("LCTH", "LEPC", "HASP", "TRBL", "SPOW", "KIWA", "BITH")
+
+to_plot <- dis_summary[which(dis_summary$Code %in% sp), ]
+
+species_plot <- ggplot(data = to_plot, aes(x = Code, y = exp(mean),
+                                           ymin = exp(q5), ymax = exp(q95))) +
+  geom_point() + 
+  geom_errorbar() +
+  xlab("Species") +
+  ylab("EDR") +
+  geom_text(aes(y = exp(q95) + 0.25, label = N)) +
+  # ylim(0,2.0) +
+  NULL
 
 ####### Output ####################################
 
-png("plots/distance_comparison.png",
+png("output/plots/distance_1vs1.png",
     width = 6, height = 4, res = 600, units = "in")
-print(comparison_plot)
+print(single_vs_multi_plot)
+dev.off()
+
+png("output/plots/distance_sd_plot.png",
+    width = 6, height = 4, res = 600, units = "in")
+print(sd_comp_plot)
+dev.off()
+
+png("output/plots/distance_predictions_plot.png",
+    width = 6, height = 4, res = 600, units = "in")
+print(species_plot)
 dev.off()
