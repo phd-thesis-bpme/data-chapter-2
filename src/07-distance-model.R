b4936ee04136fa535b25e7694fe54d72ad31f461
Brandon Edwards <brandon.edwards144@gmail.com>
2023-05-23 14:35:27 +0100

    heavily subsetting the data and change to null/species only model (no information sharing

--- src/07-distance-model.R
+++ src/07-distance-model.R
@@ -8,6 +8,7 @@
 ####### Import Libraries and External Files #######
 
 library(cmdstanr)
+library(dplyr)
 
 ####### Load Data #################################
 
@@ -28,17 +29,67 @@ n_chains <- 4
 refresh <- 10
 threads_per_chain <- 3
 
+####### Subset Data for Testing ###################
+
+orig_data_df <- data.frame(species = distance_stan_data$sp_list[,1],
+                           sp_n = distance_stan_data$species)
+### NOTE there are only 322 species in this list.
+### is n_species wrong (== 323)
+sp_df <- orig_data_df %>%
+  group_by(species,sp_n) %>%
+  summarise(n_counts = n())
+
+sub_data <- data.frame(mig_strat = distance_stan_data$mig_strat,
+                       habitat = distance_stan_data$habitat,
+                       pitch = distance_stan_data$pitch,
+                       mass = distance_stan_data$mass,
+                       sp_n = 1:length(distance_stan_data$mig_strat))
+sp_df <- left_join(sp_df,sub_data)
+
+sps <- c("GRSP","WOTH","BCCH","WTSP","BAOR","EAME","HAWO","GCFL","BOBO")
+sp_sel <- sp_df[which(sp_df$species %in% sps),]
+sp_sel[,"new_sp_n"] <- 1:length(sps) # new species indicators
+
+#identify which of the original data objects should be retained
+data_sel <- which(orig_data_df$sp_n %in% sp_sel$sp_n)
+sp_data_repl <- orig_data_df %>%
+  left_join(.,sp_sel,
+            by = "sp_n")
+
+distance_stan_data2 <- distance_stan_data
+distance_stan_data2$bands_per_sample <- distance_stan_data2$bands_per_sample[data_sel]
+distance_stan_data2$abund_per_band <- distance_stan_data2$abund_per_band[data_sel,]
+distance_stan_data2$max_dist <- distance_stan_data2$max_dist[data_sel,]
+distance_stan_data2$species <- sp_data_repl[data_sel,"new_sp_n"]
+distance_stan_data2$mig_strat <- as.integer(unlist(sp_sel[,"mig_strat"]))
+distance_stan_data2$habitat <- as.integer(unlist(sp_sel[,"habitat"]))
+distance_stan_data2$mass <- as.numeric(unlist(sp_sel[,"mass"]))
+distance_stan_data2$pitch <- as.numeric(unlist(sp_sel[,"pitch"]))
+
+distance_stan_data2$grainsize <- 1
+distance_stan_data2$n_species <- length(sps)
+distance_stan_data2$n_samples <- length(data_sel)
+
+distance_stan_data2$sp_list <- NULL
+distance_stan_data2$phylo_corr <- NULL
+distance_stan_data2$max_dist <- distance_stan_data2$max_dist / 100
+
+
 ####### Run Model #################################
 
 # get rid of centre/scale attributes for modelling
-distance_stan_data$pitch <- distance_stan_data$pitch[,1]
-distance_stan_data$mass <- distance_stan_data$mass[,1]
+distance_stan_data2$pitch <- NULL#distance_stan_data$pitch[,1]
+distance_stan_data2$mass <- NULL#distance_stan_data$mass[,1]
+distance_stan_data2$n_mig_strat <- NULL
+distance_stan_data2$mig_strat <- NULL
+distance_stan_data2$n_habitat <- NULL
+distance_stan_data2$habitat <- NULL
 
 model_file <- cmdstan_model(stan_file = "models/distance.stan",
                             cpp_options = list(stan_threads = TRUE))
 
 stan_run <- model_file$sample(
-  data = distance_stan_data,
+  data = distance_stan_data2,
   iter_warmup = n_warmup,
   iter_sampling = n_iter,
   chains = n_chains,
