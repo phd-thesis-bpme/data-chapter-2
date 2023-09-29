54a794ae704441e08c9866f72521204184391901
Brandon Edwards <brandon.edwards144@gmail.com>
2023-09-29 15:06:33 -0400

    clean up code and stage the mixed modelling 'real' run on ncc4 server

--- src/07-distance-model.R
+++ src/07-distance-model.R
@@ -11,6 +11,7 @@ library(cmdstanr)
 library(dplyr)
 
 source("src/functions/generate-distance-inits.R")
+source("src/functions/subset-distance-data.R")
 
 ####### Load Data #################################
 
@@ -23,78 +24,10 @@ n_iter <- 2000
 n_warmup <- 1000
 n_chains <- 4
 refresh <- 10
-threads_per_chain <- 2
+threads_per_chain <- 7
 
-# ####### Subset Data for Testing ###################
-# 
-# orig_data_df <- data.frame(species = distance_stan_data$sp_list[,1],
-#                            sp_n = distance_stan_data$species)
-# ### NOTE there are only 322 species in this list.
-# ### is n_species wrong (== 323)
-# sp_df <- orig_data_df %>%
-#   group_by(species,sp_n) %>%
-#   summarise(n_counts = n())
-# 
-# sub_data <- data.frame(mig_strat = distance_stan_data$mig_strat,
-#                        habitat = distance_stan_data$habitat,
-#                        pitch = distance_stan_data$pitch,
-#                        mass = distance_stan_data$mass,
-#                        sp_n = 1:length(distance_stan_data$mig_strat))
-# sp_df <- left_join(sp_df,sub_data)
-# 
-# sps <- c("GRSP",
-#    "WOTH",
-#   # "BCCH",
-#   #"WTSP"
-#   # "FLSJ",
-#   # "KIWA",
-#    "HASP"
-#   # "SPOW",
-#   # "LEPC",
-#   # "GRSG",
-#   # "WTPT",
-#   # "EASO",
-#   # "BCRF",
-#   # "MEJA",
-#   # "BOSP",
-#   # "RFWA"
-#   # "BAOR",
-#   # "EAME",
-#   # "HAWO",
-#   # "GCFL",
-#  # "BOBO"
-# )
-# sp_sel <- sp_df[which(sp_df$species %in% sps),]
-# sp_sel[,"new_sp_n"] <- 1:length(sps) # new species indicators
-# 
-# #identify which of the original data objects should be retained
-# data_sel <- which(orig_data_df$sp_n %in% sp_sel$sp_n)
-# sp_data_repl <- orig_data_df %>%
-#   left_join(.,sp_sel,
-#             by = "sp_n")
-# 
-# distance_stan_data2 <- distance_stan_data; rm(distance_stan_data); gc();
-# distance_stan_data2$bands_per_sample <- distance_stan_data2$bands_per_sample[data_sel]
-# distance_stan_data2$abund_per_band <- distance_stan_data2$abund_per_band[data_sel,]
-# distance_stan_data2$max_dist <- distance_stan_data2$max_dist[data_sel,]
-# distance_stan_data2$species <- sp_data_repl[data_sel,"new_sp_n"]
-# distance_stan_data2$mig_strat <- as.integer(unlist(sp_sel[,"mig_strat"]))
-# distance_stan_data2$habitat <- as.integer(unlist(sp_sel[,"habitat"]))
-# distance_stan_data2$mass <- as.numeric(unlist(sp_sel[,"mass"]))
-# distance_stan_data2$pitch <- as.numeric(unlist(sp_sel[,"pitch"]))
-# 
-# distance_stan_data2$grainsize <- 1
-# distance_stan_data2$n_species <- length(sps)
-# distance_stan_data2$n_samples <- length(data_sel)
-# 
-# distance_stan_data2$sp_list <- NULL
-# distance_stan_data2$phylo_corr <- NULL
-# distance_stan_data2$max_dist <- distance_stan_data2$max_dist / 100
-# 
-# distance_stan_data2$species_cp <- c(1,3)
-# distance_stan_data2$species_ncp <- c(2)
-# distance_stan_data2$n_species_cp <- 2
-# distance_stan_data2$n_species_ncp <- 1
+#' If one wanted to subset the data for certain species, here is where it should be done
+#' using the subset_distance_data() function
 
 distance_stan_data$grainsize <- 1
 distance_stan_data$lambda <- 0
