0fb1975049dbe5ace578f6801547a81185605873
Brandon Edwards <brandon.edwards144@gmail.com>
2023-10-18 16:49:38 -0400

    update cross validation post hoc analysis for distance model to match current cross validation methods

--- src/scripts/distance/7-distance-cv-analysis.R
+++ src/scripts/distance/7-distance-cv-analysis.R
@@ -21,6 +21,7 @@ source("src/functions/dis-lik.R")
 
 cv_folds <- read.csv("data/generated/distance_cv_folds.csv")
 load("data/generated/distance_stan_data.rda")
+traits <- read.csv("data/raw/traits.csv")
 
 ####### Data Wrangling ############################
 
@@ -33,58 +34,120 @@ dis_data <- subset_distance_data(distance_stan_data = distance_stan_data,
                                                pred_drops))
 rm(distance_stan_data)
 
-cv_folds$Index <- seq(1, nrow(cv_folds))
-cv_folds$EDR <- NA
+####### Main Code #################################
 
-for (i in 1:max(cv_folds$cv_fold))
+#' Create dataframe that will connect species, data point,
+#' and lppd
+lppd <- data.frame(Species = cv_folds$Species,
+                   CV_Fold = cv_folds$cv_fold,
+                   SS_LPPD = NA,
+                   MS_LPPD = NA,
+                   Difference = NA)
+
+for (f in 1:max(cv_folds$cv_fold))
 {
-  fold <- readRDS(paste0("output/model_runs/cv_distance/fold_",
-                         i,
-                         ".RDS"))
+  # Extract dataframe of tau from single species and multi species model
+  ss_mod <- readRDS(paste0("output/model_runs/cv_distance/ss_fold_",
+                           f,
+                           ".RDS"))
+  ms_mod <- readRDS(paste0("output/model_runs/cv_distance/ms_fold_",
+                           f,
+                           ".RDS"))
   
-  # Add the mean estimated EDR to the dataframe above
-  log_tau_df <- fold$summary("log_tau")
-  fold_indices <- which(cv_folds$cv_fold == i)
-  cv_folds[fold_indices, "EDR"] <- exp(log_tau_df[fold_indices, "mean"]) 
+  ss_tau_summary <- ss_mod$summary("log_tau")
+  ms_tau_summary <- ms_mod$summary("log_tau")
   
-  # Now calculate the median LPMF for each species
-  sp_fold <- cv_folds[which(cv_folds$cv_fold %in% i), c("Species", "Index")]
+  # Subset the data to just the test set
+  data_indices <- which(lppd$CV_Fold == f)
+  abund <- dis_data$abund_per_band[data_indices, ]
+  bands <- dis_data$bands_per_sample[data_indices]
+  max_dist <- dis_data$max_dist[data_indices, ] / 100
+  species <- dis_data$species[data_indices]
+  max_intervals <- dis_data$max_intervals
   
-  for (sp_index %in% sp_fold$Index)
+  tau_ss <- exp(ss_tau_summary$mean[species])
+  tau_ms <- exp(ms_tau_summary$mean[species])
+  
+  # Check to see if we can drop any training columns (i.e. if bands per sample is smaller)
+  if (max(bands) < max_intervals)
   {
-    data_indices <- which(dis_data$species == sp_index)
-    abund <- dis_data$abund_per_band[data_indices, ]
-    bands <- dis_data$bands_per_sample[data_indices]
-    max_dist <- dis_data$max_dist[data_indices, ] / 100
-    max_intervals <- dis_data$max_intervals
-    
-    # Check to see if we can drop any training columns (i.e. if bands per sample is smaller)
-    if (max(bands) < max_intervals)
-    {
-      max_intervals <- max(bands)
-      abund <- abund[, 1:max_intervals]
-      max_dist <- max_dist[, 1:max_intervals]
-    }
-    
-    tau_df <- fold$draws(variables = paste0("log_tau[",
-                                            sp_index,
-                                            "]"),
-                         format = "df")
-    
-    loglik_vector <- vector(mode = "double", length = nrow(tau_df))
-    
-    # This can probably be vectorized with apply() or something.
-    for (j in 1:length(loglik_vector))
-    {
-      loglik_vector[j] <- median(dis_lik(abund = abund,
-              bands = bands,
-              max_dist = max_dist,
-              max_intervals = max_intervals,
-              tau = exp(tau_df$`log_tau[7]`[j])))
-    }
-    
-    #' TO DO: Need to either find a way to keep track of the loglik_vector for
-    #' each species (perhaps as a n_draws X n_species matrix), or just track the
-    #' mean/median and 95% intervals of those vectors?
+    max_intervals <- max(bands)
+    abund <- abund[, 1:max_intervals]
+    max_dist <- max_dist[, 1:max_intervals]
   }
+  
+  lp_ss <- dis_lik(abund = abund,
+                   bands = bands,
+                   max_dist = max_dist,
+                   max_intervals = max_intervals,
+                   tau = tau_ss)
+  
+  lp_ms <- dis_lik(abund = abund,
+                   bands = bands,
+                   max_dist = max_dist,
+                   max_intervals = max_intervals,
+                   tau = tau_ms)
+  
+  lppd$SS_LPPD[data_indices] <- lp_ss
+  lppd$MS_LPPD[data_indices] <- lp_ms
 }
+
+lppd$Difference <- lppd$MS_LPPD - lppd$SS_LPPD
+
+lppd_summary <- data.frame(Species = unique(lppd$Species),
+                           Mean_Difference = NA,
+                           StdErr = NA,
+                           Model_Preference = NA)
+
+sp_sample_size <- data.frame(table(dis_data$sp_list))
+names(sp_sample_size) <- c("Species", "N")
+lppd_summary <- dplyr::left_join(lppd_summary, sp_sample_size, by = "Species")
+
+for (i in 1:nrow(lppd_summary))
+{
+  sp <- lppd_summary$Species[i]
+  lppd_summary$Mean_Difference[i] <- mean(lppd[which(lppd$Species == sp),
+                                               "Difference"])
+  lppd_summary$StdErr[i] <- sqrt(var(lppd[which(lppd$Species == sp),
+                                          "Difference"])) / lppd_summary$N[i]
+  
+}
+
+lppd_summary[which((lppd_summary$Mean_Difference - lppd_summary$StdErr < 0) &
+                     (lppd_summary$Mean_Difference + lppd_summary$StdErr < 0)),
+             "Model_Preference"] <- "SS"
+
+lppd_summary[which((lppd_summary$Mean_Difference - lppd_summary$StdErr > 0) &
+                     (lppd_summary$Mean_Difference + lppd_summary$StdErr > 0)),
+             "Model_Preference"] <- "MS"
+
+preference <- data.frame(table(lppd_summary$Model_Preference))
+
+(lppd_difference_plot <- ggplot(data = lppd_summary, aes(x = Species, y = Mean_Difference)) +
+    geom_errorbar(aes(ymin = Mean_Difference - StdErr, ymax = Mean_Difference + StdErr)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    #coord_flip() +
+    NULL)
+
+(lppd_diff_vs_n <- ggplot(data = lppd_summary, aes(x = N, y = Mean_Difference)) +
+    geom_point(aes(color = Model_Preference)) +
+    stat_smooth(method = "lm", 
+                formula = y ~ x, 
+                geom = "smooth") +  
+    NULL
+)
+
+####### Output ####################################
+
+write.table(lppd, file = "data/generated/lppd_distance.csv", sep = ",", row.names = FALSE)
+write.table(lppd_summary, file = "data/generated/lppd_summary_distance.csv", sep = ",", row.names = FALSE)
+
+png(filename = "output/plots/cv_distance.png",
+    width = 20, height = 6, units = "in", res = 300)
+print(lppd_difference_plot)
+dev.off()
+
+png(filename = "output/plots/cv_distance_diff_vs_n.png",
+    width = 10, height = 6, units = "in", res = 300)
+print(lppd_diff_vs_n)
+dev.off()
