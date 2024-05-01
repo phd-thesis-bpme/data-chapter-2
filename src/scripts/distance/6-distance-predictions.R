54e847f94ce6f93f95dfde884a0d025afcf13a3a
Brandon Edwards <brandon.edwards144@gmail.com>
2024-05-01 14:56:57 -0400

    udpates to distance prediction script

--- src/scripts/distance/6-distance-predictions.R
+++ src/scripts/distance/6-distance-predictions.R
@@ -3,19 +3,17 @@
 # Multi-species QPAD Detectability
 # 6-distance-predictions.R
 # Created October 2022
-# Last Updated October 2023
+# Last Updated April 2024
 
 ####### Import Libraries and External Files #######
 
 library(cmdstanr)
-library(dplyr)
 
 source("src/functions/generate-distance-inits.R")
-source("src/functions/subset-distance-data.R")
 
 ####### Load Data #################################
 
-load("data/generated/distance_stan_data.rda")
+load("data/generated/distance_stan_data_pred.rda")
 
 ####### Set Constants #############################
 
@@ -24,26 +22,24 @@ n_iter <- 2000
 n_warmup <- 1000
 n_chains <- 4
 refresh <- 10
-threads_per_chain <- 7
+threads_per_chain <- 4
 
-distance_stan_data$grainsize <- 1
+distance_stan_data_pred$grainsize <- 1
 
 inits <- generate_distance_inits(n_chains = n_chains,
                                  napops_skip = c("BITH", "HASP", "KIWA", "LCTH", "LEPC", "SPOW"),
-                                 sp_list = distance_stan_data$sp_all,
-                                 param = "cp",
-                                 species_cp = distance_stan_data$species_cp,
-                                 species_ncp = distance_stan_data$species_ncp)
+                                 sp_list = distance_stan_data_pred$sp_all,
+                                 param = "cp")
 
-distance_stan_data$sp_list <- NULL
-distance_stan_data$sp_all <- NULL
+distance_stan_data_pred$sp_list <- NULL
+distance_stan_data_pred$sp_all <- NULL
 
 # Scale the maximum distances for computational ease
-distance_stan_data$max_dist <- distance_stan_data$max_dist / 100
+distance_stan_data_pred$max_dist <- distance_stan_data_pred$max_dist / 100
 
 # get rid of centre/scale attributes for modelling
-distance_stan_data$pitch <- distance_stan_data$pitch[,1]
-distance_stan_data$mass <- distance_stan_data$mass[,1]
+distance_stan_data_pred$pitch <- distance_stan_data_pred$pitch[,1]
+distance_stan_data_pred$mass <- distance_stan_data_pred$mass[,1]
 
 ####### Run Model #################################
 
@@ -51,14 +47,13 @@ model_file <- cmdstan_model(stan_file = "models/distance_cp.stan",
                             cpp_options = list(stan_threads = TRUE))
 
 stan_run <- model_file$sample(
-  data = distance_stan_data,
+  data = distance_stan_data_pred,
   iter_warmup = n_warmup,
   iter_sampling = n_iter,
   chains = n_chains,
   parallel_chains = n_chains,
   refresh = refresh,
   threads_per_chain = threads_per_chain,
-  output_dir = "output/model_runs/stan_output/",
   init = inits
 )
 
